d1ea1f9dcf8980b136d2f438116afa7926071f81
ikenley <ikenley6@gmail.com>
2024-08-09 05:08:42 -0400

    ImageGeneratorService

--- story-generator/src/ai/TextGenerator.ts
+++ story-generator/src/ai/TextGenerator.ts
@@ -1,37 +1,27 @@
-import { readFileSync } from "fs";
 import { randomUUID } from "crypto";
-import { writeFile } from "fs/promises";
-import * as path from "path";
 import {
   BedrockRuntimeClient,
   InvokeModelCommand,
 } from "@aws-sdk/client-bedrock-runtime";
-import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
-import { ConfigOptions } from "../config/ConfigOptions";
-import { getS3DatePrefix } from "./s3Util";
+import FileService from "src/s3/FileService";
 
-type TextGeneratorResponse = {
+export type TextGeneratorResponse = {
   jobId: string;
   s3Bucket: string;
   s3Key: string;
   s3Uri: string;
 };
 
-type S3Result = { s3Bucket: string; s3Key: string; s3Uri: string };
-
 export default class ImageGeneratorService {
-  private config: ConfigOptions;
   private bedrockRuntimeClient: BedrockRuntimeClient;
-  private s3Client: S3Client;
+  private fileService: FileService;
 
   constructor(
-    config: ConfigOptions,
     bedrockRuntimeClient: BedrockRuntimeClient,
-    s3Client: S3Client
+    fileService: FileService
   ) {
-    this.config = config;
     this.bedrockRuntimeClient = bedrockRuntimeClient;
-    this.s3Client = s3Client;
+    this.fileService = fileService;
   }
 
   /** Generate an image based on a prompt, save it to S3, and send image link. */
@@ -44,9 +34,9 @@ export default class ImageGeneratorService {
 
     const fileId = randomUUID();
 
-    const filePath = await this.writeToJsonFile(fileId, lines);
+    const filePath = await this.fileService.writeToJsonFile(fileId, lines);
 
-    const s3Result = await this.uploadToS3(fileId, filePath);
+    const s3Result = await this.fileService.uploadToS3(fileId, filePath);
 
     //return s3Result;
     return { jobId, ...s3Result };
@@ -98,44 +88,5 @@ It has the title "${title}". It is about ${desription}.`,
     const allLines = modelTextResponse.split(/\r?\n/) || [];
     const last4Lines = allLines.slice(-4);
     return last4Lines;
-  }
-
-  /** Writes lines to a local JSON file.
-   * Returns the file path.
-   */
-  private async writeToJsonFile(
-    fileId: string,
-    lines: string[]
-  ): Promise<string> {
-    try {
-      const filePath = path.join("/tmp", `${fileId}.json`);
-      const jsonString = JSON.stringify(lines, null, 2);
-      await writeFile(filePath, jsonString);
-      return filePath;
-    } catch (error: any) {
-      console.error("Error parsing JSON:", JSON.stringify(error));
-      throw new Error(error);
-    }
-  }
-
-  async uploadToS3(fileId: string, filePath: string): Promise<S3Result> {
-    const fileContent = readFileSync(filePath); // This is inefficient, but works for small files
-    const datePrefix = getS3DatePrefix();
-    const s3Bucket = this.config.s3.dataLake.bucketName;
-    const s3Key = `${this.config.s3.dataLake.keyPrefix}/${datePrefix}/${fileId}.json`;
-    const input = {
-      Body: fileContent,
-      Bucket: s3Bucket,
-      Key: s3Key,
-    };
-    console.log(
-      "uploadToS3",
-      JSON.stringify({ bucket: input.Bucket, s3Key: input.Key })
-    );
-    const command = new PutObjectCommand(input);
-    await this.s3Client.send(command);
-
-    const s3Uri = `s3://${s3Bucket}/${s3Key}`;
-    return { s3Bucket, s3Key, s3Uri };
   }
 }
--- story-generator/src/lambda_function.ts
+++ story-generator/src/lambda_function.ts
@@ -1,9 +1,11 @@
 import { randomUUID } from "crypto";
 import { S3Client } from "@aws-sdk/client-s3";
 import { Handler } from "aws-lambda";
-import { getConfigOptions } from "./config/ConfigOptions";
-import TextGenerator from "./ai/TextGenerator";
+import { ConfigOptions, getConfigOptions } from "./config/ConfigOptions";
+import TextGenerator, { TextGeneratorResponse } from "./ai/TextGenerator";
+import FileService from "./s3/FileService";
 import { BedrockRuntimeClient } from "@aws-sdk/client-bedrock-runtime";
+import ImageGeneratorService from "./ai/TextGenerator";
 
 export const handler: Handler = async (event, _context, callback) => {
   console.log(`event= ${JSON.stringify(event)}`);
@@ -12,27 +14,80 @@ export const handler: Handler = async (event, _context, callback) => {
 
   let result = {};
   if (event.Command === "GenerateText") {
-    const bedrockRuntimeClient = new BedrockRuntimeClient({
-      region: config.aws.region,
-    });
-    const s3Client = new S3Client({
-      region: config.aws.region,
-    });
-    const jobId = randomUUID();
     const title = event.Title;
     const description = event.Description;
-
-    const textGenerator = new TextGenerator(
+    result = await generateText(config, title, description);
+  } else if (event.Command === "GenerateImages") {
+    const jobId = event.JobId;
+    const title = event.Title;
+    const description = event.Description;
+    const linesS3Bucket = event.LinesS3Bucket;
+    const linesS3Key = event.linesS3Key;
+    result = await generateImages(
       config,
-      bedrockRuntimeClient,
-      s3Client
+      jobId,
+      title,
+      description,
+      linesS3Bucket,
+      linesS3Key
     );
-    result = await textGenerator.generate(jobId, title, description);
   }
 
   console.log(`result= ${JSON.stringify(result)}`);
 
   callback(null, result);
+};
+
+const generateText = async (
+  config: ConfigOptions,
+  title: string,
+  description: string
+): Promise<TextGeneratorResponse> => {
+  const bedrockRuntimeClient = new BedrockRuntimeClient({
+    region: config.aws.region,
+  });
+  const s3Client = new S3Client({
+    region: config.aws.region,
+  });
+  const jobId = randomUUID();
+
+  const fileService = new FileService(config, s3Client);
+  const textGenerator = new TextGenerator(bedrockRuntimeClient, fileService);
+  const result = await textGenerator.generate(jobId, title, description);
+
+  return result;
+};
+
+const generateImages = async (
+  config: ConfigOptions,
+  jobId: string,
+  title: string,
+  description: string,
+  linesS3Bucket: string,
+  linesS3Key: string
+) => {
+  const bedrockRuntimeClient = new BedrockRuntimeClient({
+    region: config.aws.region,
+  });
+  const s3Client = new S3Client({
+    region: config.aws.region,
+  });
+
+  const fileService = new FileService(config, s3Client);
+  const imageGenerator = new ImageGeneratorService(
+    bedrockRuntimeClient,
+    fileService
+  );
+
+  const result = await imageGenerator.generate(
+    jobId,
+    title,
+    description,
+    linesS3Bucket,
+    linesS3Key
+  );
+
+  return result;
 };
 
 export default handler;
