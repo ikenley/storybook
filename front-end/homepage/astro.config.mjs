0d11821a685d3f409e7da98f950edb0724c4410c
Ian Kenley <ikenley6@gmail.com>
2025-04-08 19:05:11 -0400

    homepage

---
+++ front-end/homepage/astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from "astro/config";
+
+const base = "/home";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://static.ikenley.com",
+  base: base,
+});
